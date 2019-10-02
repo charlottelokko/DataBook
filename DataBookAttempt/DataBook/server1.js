211b407f8adaeb0fbe85c53237fd9b02ff09e57c
Charlotte Lokko <littlelokko@hotmail.com>
2019-10-02 14:05:13 +0100

    restructure and making pages run on server

--- DataBookAttempt/DataBook/server1.js
+++ DataBookAttempt/DataBook/server1.js
@@ -4,11 +4,8 @@ var app = express();
 
 app.engine('html', require('ejs').renderFile);  //allows html pages and injection of data into them 
 
-app.use(express.static(__dirname + '/css'));
+app.use(express.static('public'));
 
-app.get("/",function(req,res){
-    res.render(__dirname + "/index.html");
-});
 
 var server = app.listen(process.env.PORT || 8081, function () {
     var host = server.address().address;
@@ -18,3 +15,26 @@ var server = app.listen(process.env.PORT || 8081, function () {
 });
 
 //app.listen(port, () => console.log(`Example app listening on port ${port}!.... http://127.0.0.1:${port}/`))
+
+app.get("/",function(req,res){
+    res.render(__dirname + "/views/index.html");
+    // res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/charts.html",function(req,res){
+    res.sendFile(__dirname + "/views/charts.html");
+});
+
+app.get("/tables.html",function(req,res){
+    res.sendFile(__dirname + "/views/tables.html");
+});
+
+app.get('/js/WeatherChart.js',
+    function(req,res){
+        res.sendFile(path.join(__dirname + '/js/WeatherChart.js'));
+    }); 
+
+app.get('/js/WeatherChart.js',
+    function(req,res){
+        res.sendFile(path.join(__dirname + '/js/datatables-demo.js'));
+    }); 
